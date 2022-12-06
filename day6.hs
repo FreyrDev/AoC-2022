efb705d71fdeeafd3a1c1bacf98e1e4dc7b4e975
Freyr <95385132+FreyrDev@users.noreply.github.com>
2022-12-06 22:04:22 +0000

    Cleaner day 6 using tails

--- day6.hs
+++ day6.hs
@@ -1,9 +1,9 @@
-import Data.List (elemIndex, nub)
+import Data.List (elemIndex, nub, tails)
 import Data.Maybe (fromJust)
 import Control.Arrow ((&&&))
 
 uniqueSeqOf :: Ord a => Int -> [a] -> Int
-uniqueSeqOf n xs = (+n) . fromJust . elemIndex n . map (length . nub . take n . (`drop` xs)) $ [0..]
+uniqueSeqOf n = (+n) . fromJust . elemIndex n . map (length . nub . take n) . tails
 
 main :: IO ()
 main = print . (uniqueSeqOf 4 &&& uniqueSeqOf 14) =<< readFile "inputs/day6.txt"
