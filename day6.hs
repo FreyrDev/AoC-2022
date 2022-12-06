c1fdfc91d2fe9abbb63e59f843296017963ca9d0
Freyr <95385132+FreyrDev@users.noreply.github.com>
2022-12-06 11:53:33 +0000

    Cleaner day 6

--- day6.hs
+++ day6.hs
@@ -1,14 +1,9 @@
-import Data.Function ((&))
-import Data.Set (toList, fromList)
+import Data.List (elemIndex, nub)
+import Data.Maybe (fromJust)
 import Control.Arrow ((&&&))
 
-
-uniqueSeqOf :: Int -> [Char] -> Int
-uniqueSeqOf n = loop n 0 . splitAt n
-  where loop i uniq (xs,y:ys) 
-          | uniq == n = i-1
-          | otherwise = loop (i+1)  (uniqueElems xs) (tail xs ++ [y], ys)
-        uniqueElems = length . toList . fromList
+uniqueSeqOf :: Ord a => Int -> [a] -> Int
+uniqueSeqOf n xs = (+n) . fromJust . elemIndex n . map (length . nub . take n . (`drop` xs)) $ [0..]
 
 main :: IO ()
 main = print . (uniqueSeqOf 4 &&& uniqueSeqOf 14) =<< readFile "inputs/day6.txt"
