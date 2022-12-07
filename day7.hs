3bed54041e1432140ca60fd64a6d2d8aa68606bf
Freyr <95385132+FreyrDev@users.noreply.github.com>
2022-12-07 23:52:55 +0000

    Adjustments to day 7

--- day7.hs
+++ day7.hs
@@ -3,44 +3,43 @@ import Data.List (find, isSuffixOf, sort)
 import Data.Maybe (fromJust)
 import Data.Bifunctor (second)
 import Control.Arrow ((&&&))
+import GHC.Utils.Misc (filterOut)
 
 
-data Cmd = Dir | Ls | CdUp |Cd String | F String Int
+data Cmd = Dir | Ls | CdUp |Cd String | F Int
 
-data File = File String Int
+newtype File = File {fileSize :: Int}
 
 type Path = [String]
 type PathDict = [(Path,[File])]
 
-fileSize :: File -> Int
-fileSize (File _ size) = size
-
 insertInto :: PathDict -> (Path,[File]) -> PathDict
 insertInto xs x = x : xs
 
-adjustDict :: ([File] -> [File]) -> Path -> PathDict -> PathDict
-adjustDict f path dict = insertInto (filter (not . isPath) dict) . second f . fromJust . find isPath $ dict
+insertAtPath :: Path -> ([File] -> [File]) -> PathDict -> PathDict
+insertAtPath path f = (insertInto . filterOut isPath) <*> (second f . fromJust . find isPath)
   where isPath = (path ==) . fst
 
 runCmd :: (Path, PathDict) -> Cmd -> (Path, PathDict)
-runCmd (wd, dict)  Dir     = (wd, dict)
+runCmd (wd, dict)  Dir     = (wd,      dict)
 runCmd (wd, dict)  Ls      = (wd,      dict)
 runCmd (wd, dict)  CdUp    = (tail wd, dict)
 runCmd (wd, dict) (Cd dir) = (dir:wd,  insertInto dict (dir:wd, []))
-runCmd (wd, dict) (F n sz) = (wd,      adjustDict (File n sz:) wd dict)
+runCmd (wd, dict) (F size) = (wd,      insertAtPath wd (File size:) dict)
 
 parse :: String -> PathDict
 parse = populatePathDict . map (parseCmd . words) . lines
   where populatePathDict =  snd . foldl runCmd ([],[])
-        parseCmd ["dir", _]        = Dir
-        parseCmd ["$", "ls"]       = Ls
+        parseCmd ["dir", _       ] = Dir
+        parseCmd ["$", "ls"      ] = Ls
         parseCmd ["$", "cd", ".."] = CdUp
-        parseCmd ["$", "cd", dir]  = Cd dir
-        parseCmd [size, name]      = F name (read size)
+        parseCmd ["$", "cd", dir ] = Cd dir
+        parseCmd [size, _        ] = F (read size)
 
 recursiveSize :: PathDict -> [(Path, Int)]
-recursiveSize = (map =<< flip (foldl sumIfInside)) . map (second (sum . map fileSize))
-  where sumIfInside (px,vx) (py,vy) = if px `isSuffixOf` py && px /= py then (px,vx+vy) else (px,vx)
+recursiveSize = (map =<< flip (foldl sumIfInside)) . map (second $ sum . map fileSize)
+  where sumIfInside (px,vx) (py,vy) = if px `isProperSuffixOf` py then (px,vx+vy) else (px,vx)
+        isProperSuffixOf x y = x `isSuffixOf` y && x /= y
 
 part1, part2 :: [(Path, Int)] -> Int
 part1 = sum . filter (<= 100000) . map snd
