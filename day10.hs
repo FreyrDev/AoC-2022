602d12bcc215d9a611bd2ffe5fa8fb0dc9a906b9
Freyr <95385132+FreyrDev@users.noreply.github.com>
2022-12-10 23:50:42 +0000

    Adjustments to day 10

--- day10.hs
+++ day10.hs
@@ -1,7 +1,6 @@
 import Data.Function ((&))
 import Control.Monad ((<=<))
 import Data.List.Split (chunksOf)
-import Data.List (intercalate)
 import Data.Functor ((<&>))
 
 data Instruction = Noop | Add Int deriving Show
@@ -11,21 +10,21 @@ parse = (Noop:) . (parseInstrcts . words <=< lines)
   where parseInstrcts [_]   = [Noop]
         parseInstrcts [_,v] = [Noop,Add (read v)]
 
-runInstrcts :: (Int, Int) -> [Instruction] -> [(Int, Int)]
-runInstrcts (crt,x) [] = []
-runInstrcts (crt,x) (Noop  : ins) = (crt,x)   : runInstrcts (crt+1,x) ins
-runInstrcts (crt,x) (Add v : ins) = (crt,x+v) : runInstrcts (crt+1,x+v) ins
+runInstrcts :: Int -> [Instruction] -> [Int]
+runInstrcts x [] = []
+runInstrcts x (Noop  : ins) = x   : runInstrcts x ins
+runInstrcts x (Add v : ins) = x+v : runInstrcts (x+v) ins
 
-sumSignals :: [(Int,Int)] -> Int
-sumSignals xs = sum $ map ((*) <*> snd . (xs !!) . subtract 1) [20,60..220]
+sumSignals :: [Int] -> Int
+sumSignals xs = sum $ map ((*) =<< (xs !!) . subtract 1) [20,60..220]
 
-drawScreen :: [(Int, Int)] -> [Char]
-drawScreen = splitLines . (drawPixel <=< init)
-  where drawPixel (crt,x) = if abs (crt`mod`40 - x) <= 1 then "@@" else "  "
-        splitLines = intercalate "\n" .  chunksOf 80
+drawScreen :: [Int] -> String
+drawScreen = splitLines . concat . zipWith drawPixel [0..]
+  where drawPixel crt x = if abs (crt`mod`40 - x) <= 1 then "@@" else "  "
+        splitLines = unlines . chunksOf 80
 
 main :: IO ()
 main = do
-  input <- readFile "inputs/day10.txt" <&> parse <&> runInstrcts (0,1)
+  input <- readFile "inputs/day10.txt" <&> parse <&> runInstrcts 1
   sumSignals input & print
-  drawScreen input & putStrLn
+  drawScreen input & putStr
