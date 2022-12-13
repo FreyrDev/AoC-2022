902c6ebd881d4d64d60658bc84f5d34fbd85e29f
Freyr <95385132+FreyrDev@users.noreply.github.com>
2022-12-13 15:18:32 +0000

    Adjustments to day 13

--- day13.hs
+++ day13.hs
@@ -1,49 +1,57 @@
-import Data.Function ((&))
-import Data.Functor ((<&>))
-import Data.List (isPrefixOf)
+import Data.Function ((&), on)
+import Data.List (isPrefixOf, sortBy, elemIndex)
 import Data.List.Split (splitOn)
-import Data.Foldable (traverse_)
+import GHC.Data.Maybe (fromJust)
+import Control.Applicative (liftA2)
 
-data Result = WrongOrder | RightOrder | NoDecision deriving (Eq,Enum)
-data Tree a = Leaf a | Branch [Tree a] deriving Read
+
+data Tree a = Leaf a | Node [Tree a] deriving (Eq,Read)
 
 instance Show a => Show (Tree a) where
   show :: Show a => Tree a -> String
-  show (Branch xs) = show xs
-  show (Leaf x) = show x
+  show (Node xs) = show xs
+  show (Leaf x)  = show x
 
-replace :: Eq a => [a] -> [a] -> [a] -> [a]
-replace  _ _ [] = []
-replace [] _ xs = xs
-replace this that xs = 
+infix 4 >->
+(>->) :: Eq a => [a] -> [a] -> [a] -> [a]
+(_  >-> _) [] = []
+([] >-> _) xs = xs
+(this >-> that) xs = 
   if this `isPrefixOf` xs
-  then that   ++ replace this that (drop (length this) xs)
-  else head xs : replace this that (tail xs)
+  then that   ++ (this >-> that) (drop (length this) xs)
+  else head xs : (this >-> that) (tail xs)
 
 parse :: String -> [([Tree Int],[Tree Int])]
-parse = map ((\(Branch x, Branch y) -> (x, y)) . packetPair) . splitOn [""] . lines . treeString
-  where treeString input = foldl leafString input [0..9] & replace "1Leaf " "1" & replace "[" "Branch [" 
-        leafString acc x = replace (show x) ("Leaf " ++ show x) acc
+parse = map ((\(Node x, Node y) -> (x, y)) . packetPair) . splitOn [""] . lines . treeStr
+  where treeStr input = foldl leafStr input [0..9] & ("1Leaf " >-> "1") & ("[" >-> "Node [")
+        leafStr acc x = (show x >-> "Leaf " ++ show x) acc
         packetPair [x,y] = (read x, read y)
 
-thenIfNecessary :: Result -> Result -> Result
-thenIfNecessary x y = if x == NoDecision then y else x
+addDividers :: [([Tree Int],[Tree Int])] -> [[Tree Int]]
+addDividers = ([Node [Leaf 6]]:) . ([Node [Leaf 2]]:) . concatMap (\(x,y) -> [x,y])
 
-checkOrder :: [Tree Int] -> [Tree Int] -> Result
-checkOrder [] [] = NoDecision
-checkOrder [] _  = RightOrder
-checkOrder _  [] = WrongOrder
-checkOrder (Branch x:xs) (Branch y:ys) = checkOrder x y        `thenIfNecessary` checkOrder xs ys
-checkOrder (Branch x:xs)   (Leaf y:ys) = checkOrder x [Leaf y] `thenIfNecessary` checkOrder xs ys
-checkOrder   (Leaf x:xs) (Branch y:ys) = checkOrder [Leaf x] y `thenIfNecessary` checkOrder xs ys
-checkOrder   (Leaf x:xs)   (Leaf y:ys)
+thenIfNecessary :: Ordering -> Ordering -> Ordering
+thenIfNecessary x y = if x == EQ then y else x
+
+checkOrder :: [Tree Int] -> [Tree Int] -> Ordering
+checkOrder [] [] = EQ
+checkOrder [] _  = LT
+checkOrder _  [] = GT
+checkOrder (Node x:xs) (Node y:ys) = checkOrder x y        `thenIfNecessary` checkOrder xs ys
+checkOrder (Node x:xs) (Leaf y:ys) = checkOrder x [Leaf y] `thenIfNecessary` checkOrder xs ys
+checkOrder (Leaf x:xs) (Node y:ys) = checkOrder [Leaf x] y `thenIfNecessary` checkOrder xs ys
+checkOrder (Leaf x:xs) (Leaf y:ys)
   | x == y    = checkOrder xs ys
-  | otherwise = toEnum . fromEnum $ x < y
+  | otherwise = compare x y
 
-countIndicies :: [Result] -> Int
-countIndicies = sum . zipWith (*) [1..] . map fromEnum
+countRightOrder :: [Ordering] -> Int
+countRightOrder = sum . zipWith (*) [1..] . map (abs . (`div` 2) . subtract 2 . fromEnum)
+
+findDecoder :: [[Tree Int]] -> Int
+findDecoder = ((*) `on` succ.fromJust) <$> elemIndex [Node [Leaf 2]] <*> elemIndex [Node [Leaf 6]]
 
 main :: IO ()
 main = do
-  input <- readFile "inputs/day13.txt"
-  parse input & map (uncurry checkOrder) & countIndicies & print
+  input <- parse <$> readFile "inputs/day13.txt"
+  map (uncurry checkOrder) input & countRightOrder & print
+  addDividers input & sortBy checkOrder & findDecoder & print
